dc18487d02e26f9ef9109b95589a2865b4b9e722
kopham <khoi.2.pham@ucdenver.edu>
2024-04-09 00:22:53 -0600

    new changes

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const connection_url = 'mongodb+srv://khoi2pham:matnaden@cluster0.exlyxrp.mongod
 
 //Middleware
 app.use(express.json())
-app.use(Cors)
+app.use(Cors())
 
 //DB Config
-mongoose.connect(connection_url
-    //useNewUrlParser: true,
-    // useCreateIndex: true,
-    //useUnifiedTopology: true
-)
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true
+})
 .then(() => {
     console.log("Connected to MongoDB");
 })
@@ -28,28 +28,24 @@ mongoose.connect(connection_url
 //API Endpoints
 app.get("/", (req, res) => res.status(200).send("Hello The Web Dev"))
 
-app.post('/messages/new', (req, res) => {
-    const dbMessage = req.body
-    Messages.create(dbMessage, (err, data) => {
-        if(err){
-            res.status(500).send(err)
-        }
-        else{
-            res.status(201).send(data)
-        }
-    })
+app.post('/messages/new', async (req, res) => {
+    try {
+        const dbMessage = req.body
+        const createdMessage = await Messages.create(dbMessage)
+        res.status(201).send(createdMessage)
+    }catch (err) {
+        res.status(500).send(err)
+    }
 })
 
-app.get('/messages/sync', (req, res) => {
-    Messages.find((err,data) => {
-        if(err){
-            res.status(500).send(err)
-        }
-        else{
-            res.status(200).send(data)
-        }
-    })
-})  
+app.get('/messages/sync', async (req, res) => {
+    try{
+        const messages = await Messages.find()
+        res.status(200).send(messages)
+    }catch (err){
+        res.status(500).send(err)
+    }
+})
 
 //Listener
 app.listen(port, () => console.log(`Listening on localhost: ${port}`))
